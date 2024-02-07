7ea25f5f357a77d3733a14dddffd61cc5b078cb8
Nouman Khan <nomiofficial68@gmail.com>
2024-02-07 08:55:32 +0500

    created some components

--- blog-the-mega-project/src/components/index.js
+++ blog-the-mega-project/src/components/index.js
@@ -1,3 +1,8 @@
 import Footer from './footer/Footer';
 import Header from './header/Header';
-export { Header, Footer };
+import Container from './container/Container';
+import Logo from './Logo';
+import LogoutBtn from './header/LogoutBtn';
+import Button from './Button';
+import Input from './Input';
+export { Header, Footer, Container, Logo, LogoutBtn, Button, Input };
