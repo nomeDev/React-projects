f8e59b582bc7ea8267c2661dbdb43923477dec9f
Nouman Khan <nomiofficial68@gmail.com>
2024-04-26 23:10:38 +0500

    working on footer and newsletter section

--- E-comm/src/components/Newsletter/Newsletter.jsx
+++ E-comm/src/components/Newsletter/Newsletter.jsx
@@ -1,20 +1,48 @@
-import React from 'react';
+import React from "react";
+
+import { FaFacebookF, FaInstagram } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa6";
+import { FaTwitter } from "react-icons/fa";
 
 export default function Newsletter() {
-    return <div>
-        <div className={`w-full min-h-[40vh] bg-gray-400 bg-[url(./images/newsletter-bg.jpeg)] bg-center bg-cover flex items-center justify-center`}>
-            <div className="container text-center">
-                <p>NEWSLETTER</p>
-                <h3>SIGN UP FOR LATEST UPDATES AND OFFERS</h3>
-                <div className="input">
-                    <input type="text" />
-                    <button>Subscribe</button>
-                    
-                </div>
-                <p>Will be used in accordance with out Privacy Policy</p>
-                <div className="icons">Icons here..</div>
+  return (
+    <div>
+      <div
+        className={`w-full min-h-[40vh] bg-gray-400 bg-[url(./images/newsletter-bg.jpeg)] bg-center bg-cover flex items-center justify-center`}>
+        <div className="container w-full text-center">
+          <p className="text-zinc-800">NEWSLETTER</p>
+          <h3 className="text-xl md:text-2xl font-bold my-3">
+            SIGN UP FOR LATEST UPDATES AND OFFERS
+          </h3>
+          <div className="input">
+            <input
+              className="px-2 py-[3.4px] border-[1px] border-gray-300 outline-none w-[250px] "
+              placeholder="Email Address"
+              type="text"
+            />
+            <button className="bg-purple-600 text-white px-3 py-1 border-[1] border-purple-500 h-full">
+              Subscribe
+            </button>
+          </div>
+          <p className="text-zinc-700 my-3">
+            Will be used in accordance with out Privacy Policy
+          </p>
+          <div className="icons text-[18] flex items-center justify-center gap-2">
+            <div className="w-[35px] h-[35px] rounded-full bg-zinc-800 flex items-center justify-center">
+              <FaFacebookF className="fill-white bg-zinc-800" />
             </div>
+            <div className="w-[35px] h-[35px] rounded-full bg-zinc-800 flex items-center justify-center">
+              <FaLinkedinIn className="fill-white bg-zinc-800" />
+            </div>
+            <div className="w-[35px] h-[35px] rounded-full bg-zinc-800 flex items-center justify-center">
+              <FaTwitter className="fill-white bg-zinc-800" />
+            </div>
+            <div className="w-[35px] h-[35px] rounded-full bg-zinc-800 flex items-center justify-center">
+              <FaInstagram className="fill-white bg-zinc-800" />
+            </div>
+          </div>
         </div>
-
-    </div>;
+      </div>
+    </div>
+  );
 }
